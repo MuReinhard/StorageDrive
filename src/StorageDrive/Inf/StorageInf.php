403d86b06fef845dc2593770f8f001b10a68a154
ShiOGong <2607572176@qq.com>
2018-01-23 16:57:51 +0800

    // 提交

---
+++ src/StorageDrive/Inf/StorageInf.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace StorageDrive\Inf;
+
+/**
+ * @class StorageInf
+ * @author ShiO
+ */
+
+interface StorageInf
+{
+    /**
+     * @param $key
+     * @param null $value
+     * @return mixed
+     */
+    public function set($key, $value = null);
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function get($key);
+
+    /**
+     * @return mixed
+     */
+    public function clean();
+}
